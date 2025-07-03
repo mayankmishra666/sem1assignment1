4bc7b5f27a48149b306e821fc79470331e78eb32
Mayank Mishra <itzmayankmishra1729@gmail.com>
2025-07-04 00:05:42 +0530

    Add files via upload

---
+++ assignment1.c
@@ -0,0 +1,10 @@
+// Write a C program to print your name and roll number in two separate lines.
+#include <stdio.h> 
+void main() {
+    // Print name
+    printf("Mayank Mishra\n");
+    
+    // Print roll number
+    printf("BTECH/CSE/24/057\n");
+    
+}
